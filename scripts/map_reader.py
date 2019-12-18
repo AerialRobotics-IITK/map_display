394e8b8fbddc140ffb175a36a44d40066b416a06
Kshitij1K <kabeer@iitk.ac.in>
2019-12-18 11:03:48 +0530

    Finally corrected everything

--- scripts/map_reader.py
+++ scripts/map_reader.py
@@ -4,6 +4,7 @@ import os
 import requests
 import cv2
 from std_srvs.srv import Trigger, TriggerRequest, TriggerResponse
+import math
 
 def distance(lat1, lon1, lat2, lon2):
     earthradius = 6371
@@ -37,32 +38,38 @@ if __name__ == "__main__":
     center_lon = 80.232266
     center_point = (1000,750)
     # pin_style = "50"
-    # zoom = "19"
+    zoom = 19
     # size = "2000,1500"
     f = open(os.path.expanduser('~') + '/.ros/router/gps.txt')
-    text = f.read()
-    list_words = text.split('\n')
-    num_coords = (int)(list_words[0])
-    f.close()
+    # text = f.read()
+    # list_words = text.split('\n')
+    num_coords = 0
+    # f.close()
     rate = rospy.Rate(10)
+    scaling_factor = (2**zoom)/(156543.04 * math.cos(math.radians(center_lat)))
     
     while not rospy.is_shutdown():
-        final_url = url + center + "/" + zoom + "?mapSize="+size 
+        # final_url = url + center + "/" + zoom + "?mapSize="+size 
         f = open(os.path.expanduser('~') + '/.ros/router/gps.txt')
         text = f.read()
         f.close()
         list_words = text.split('\n')
         num = (int)(list_words[0])
-        print("Hello")
+        # print("Hello")
         if (num != num_coords):
             num_coords = num
-            for i in range (1,num_coords+1):
-                map = cv2.imread (os.path.expanduser('~')+'/.ros/router/emptymap.jpg')
+            map = cv2.imread (os.path.expanduser('~')+'/.ros/router/emptymap.jpg')
+
+            for i in range (1,num_coords+1):                
                 lat,lon = list_words[i].split(' ')
-                bearing_obj = bearing(center_lat, center_lon, obj_lat, obj_lon)
-                distance_obj = distance(center_lat, center_lon, obj_lat, obj_lon)
-                point_to_mark = (int(round(centre_point[0] + scaling_factor*distance_obj*math.cos(bearing_obj))),int(round(centre_point[1] + scaling_factor*distance_obj*math.sin(bearing_obj))))
-                cv2.circle(map, point_to_mark, 5, (255,0,0))
+                bearing_obj = bearing(center_lat, center_lon, float(lat), float(lon))
+                distance_obj = distance(center_lat, center_lon, float(lat), float(lon))
+                point_to_mark = (int(round(center_point[0] + scaling_factor*distance_obj*math.sin(bearing_obj))),int(round(center_point[1] - scaling_factor*distance_obj*math.cos(bearing_obj))))
+                # point_to_mark = (int(round(center_point[1] + scaling_factor*distance_obj*math.sin(bearing_obj))),int(round(center_point[0] + scaling_factor*distance_obj*math.cos(bearing_obj))))
+                
+
+                print (point_to_mark)
+                cv2.rectangle(map, (point_to_mark[0] - 15,point_to_mark[1] - 15),(point_to_mark[0] + 15,point_to_mark[1] + 15), (0,0,255), -1)
                 # final_url = final_url + "&pushpin=" + lat + "," + lon + ";" + pin_style + ";B" + str(i)
                 
                 # final_url = final_url + "&key=" + api_key
@@ -70,9 +77,9 @@ if __name__ == "__main__":
                 # img = open (os.path.expanduser('~')+'/.ros/router/map.jpg', 'wb')
                 # img.write(final_img.content)
                 # img.close()
-                trigger_req = TriggerRequest()
-                result = image_service(trigger_req)
-                print(result)
-                print("")
             cv2.imwrite(os.path.expanduser('~')+'/.ros/router/map.jpg',map)
+            trigger_req = TriggerRequest()
+            result = image_service(trigger_req)
+            print(result)
+            print("")
         rate.sleep()
