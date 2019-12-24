37d2ea9481bfdd3130430515824a7be22b33295c
KsKab <kabeer@iitk.ac.in>
2019-12-24 19:50:50 +0530

    Commented and cleaned map_reader.py

--- scripts/map_reader.py
+++ scripts/map_reader.py
@@ -7,19 +7,26 @@ import cv2
 from std_srvs.srv import Trigger, TriggerRequest, TriggerResponse
 import math
 
+# Function for calculating distance between two points having GPS coordinates lat1,lon1 and lat2,lon2 respectively
 def distance(lat1, lon1, lat2, lon2):
     earthradius = 6371
 
+    #Converting the difference in latitudes and longitudes to radians
     dLat = math.radians(lat1 - lat2)
     dLon = math.radians(lon1 - lon2)
 
+    #Converting the latitudes to radians
     lat1 = math.radians(lat1)
     lat2 = math.radians(lat2)
 
+    #Formula for calculating angle between the radii of earth joining the two points
     a = math.sin(dLat/2) * math.sin(dLat/2) + math.sin(dLon/2) * math.sin(dLon/2) * math.cos(lat1) * math.cos(lat2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    return earthradius*c*1000
+    deltaTheta = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
 
+    #Return distance
+    return earthradius*deltaTheta*1000
+
+#Function for calculating bearing (clockwise angle from the North direction) of vector from point1(GPS coordinates lat1,lon1) to point2(GPS coordinates lat2,lon2)
 def bearing (lat1, lon1, lat2, lon2):
     lat1 = math.radians(lat1)
     lat2 = math.radians(lat2)
@@ -30,54 +37,55 @@ def bearing (lat1, lon1, lat2, lon2):
 
 
 if __name__ == "__main__":
+    #ROS variables and initializations
     rospy.init_node('image_update')
     rospy.wait_for_service('/update_image')
     image_service = rospy.ServiceProxy('/update_image', Trigger)
-    # api_key = "your-api-key-goes-here"
-    # url = "https://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial/"
+    rate = rospy.Rate(10)
+
+    #Essential Parameters:-
+    #center_lat, center_lon := GPS coordinates of the point corresponding to the center pixel on the map
+    #center_point := Pixel coordinates of the center point (width/2,height/2)
+    #zoom := Zoom level of the bing maps being used
     center_lat = 26.509590 
     center_lon = 80.226678
     center_point = (1000,750)
-    # pin_style = "50"
     zoom = 19
-    # size = "2000,1500"
-    f = open(os.path.expanduser('~') + '/.ros/router/gps.txt')
-    # text = f.read()
-    # list_words = text.split('\n')
+    
+    #num_coords stores the number of coordinate points currently in the gps.txt file
     num_coords = 0
-    # f.close()
-    rate = rospy.Rate(10)
+
+    #scaling_factor calculates the pixels/meter ratio of the given map
     scaling_factor = (2**zoom)/(156543.04 * math.cos(math.radians(center_lat)))
     
     while not rospy.is_shutdown():
-        # final_url = url + center + "/" + zoom + "?mapSize="+size 
+        #This block reads the gps.txt and stores each line in a list called list_words
         f = open(os.path.expanduser('~') + '/.ros/router/gps.txt')
         text = f.read()
         f.close()
         list_words = text.split('\n')
+
+        #If the number of coordinates (stored in num) is different than the previous number of coordinates
+        #(stored in num_coords) change num_coords to num, write the new image and call service to update 
+        #image
         num = (int)(list_words[0])
-        # print("Hello")
         if (num != num_coords):
             num_coords = num
             map = cv2.imread (os.path.expanduser('~')+'/.ros/router/emptymap.jpg')
 
-            for i in range (1,num_coords+1):                
+            for i in range (1,num_coords+1):
+                #get latitude and longitude of each point, calculate bearing and distance of that point from the center, and pixel coordinates of the point
+                #to be marked on the image                
                 lat,lon = list_words[i].split(' ')
                 bearing_obj = bearing(center_lat, center_lon, float(lat), float(lon))
                 distance_obj = distance(center_lat, center_lon, float(lat), float(lon))
                 point_to_mark = (int(round(center_point[0] + scaling_factor*distance_obj*math.sin(bearing_obj))),int(round(center_point[1] - scaling_factor*distance_obj*math.cos(bearing_obj))))
-                # point_to_mark = (int(round(center_point[1] + scaling_factor*distance_obj*math.sin(bearing_obj))),int(round(center_point[0] + scaling_factor*distance_obj*math.cos(bearing_obj))))
                 
-
+                #Print point to be marked and draw a rectangle around it
                 print (point_to_mark)
                 cv2.rectangle(map, (point_to_mark[0] - 15,point_to_mark[1] - 15),(point_to_mark[0] + 15,point_to_mark[1] + 15), (0,255,0), -1)
-                # final_url = final_url + "&pushpin=" + lat + "," + lon + ";" + pin_style + ";B" + str(i)
-                
-                # final_url = final_url + "&key=" + api_key
-                # final_img = requests.get(final_url)
-                # img = open (os.path.expanduser('~')+'/.ros/router/map.jpg', 'wb')
-                # img.write(final_img.content)
-                # img.close()
+
+            #Write the image, call the service, and print the result     
             cv2.imwrite(os.path.expanduser('~')+'/.ros/router/map.jpg',map)
             trigger_req = TriggerRequest()
             result = image_service(trigger_req)
